c9d674b9b782cf85fa6bea99731fd81a8bb68cf8
Micah Smith <micahjsmith@gmail.com>
2017-01-27 13:49:42 -0500

    Fix bugs

--- aws_util.sh
+++ aws_util.sh
@@ -5,8 +5,12 @@
 # Must populate aws CLI credentials
 # -> saved into ~/.aws
 # Must populate ec2 instance config and credentials
-# -> export AWS_EC2_USER_NAME="ec2-user" # for example
-# -> export AWS_EC2_SSH_KEY="~/.ssh/my-key.pem" # for example
+# -> export AWS_EC2_USER_NAME="ec2-user"            # for example
+# -> export AWS_EC2_SSH_KEY="$HOME/.ssh/my-key.pem" # for example.
+# In the above, use '$HOME' instead of '~'. Trust me.
+
+# Exports here
+export connect_to_instance
 
 function get_instance_id {
     local name_tag=$1
@@ -96,25 +100,26 @@ function stop_instance {
 }
 
 function connect_to_instance {
-    set -e
     if [ "$#" != "1" ]; then
         echo "usage: connect_to_instance instance_tag_name"
-        exit 1
+        return 1
     fi
 
-    if [ ( "${AWS_EC2_USER_NAME}" = "" ) \
-        -o ( "${AWS_EC2_SSH_KEY}" = "" ) \
-        -o ( !  -f "${AWS_EC2_SSH_KEY}" ) ]; then
+    eval '_USER_NAME="${AWS_EC2_USER_NAME}"'
+    eval '_SSH_KEY="${AWS_EC2_SSH_KEY}"'
+    if [ "${_USER_NAME}" = "" ] \
+       || [ "${_SSH_KEY}" = "" ] \
+       || [ ! -f "${_SSH_KEY}" ]; then
         echo "Error: populate AWS_EC2_USER_NAME and AWS_EC2_SSH_KEY variables"
-        exit 1
+        return 1
     fi
 
     local name_tag=$1
-    local instance_id=$(get_instance_id ${name_tag})
-    
+    local instance_id=$(get_instance_id ${name_tag}) || { echo "Error: Could not get instance id"; return 1; }
+
     # Restart instance is status is "stopped"
     if is_instance_stopped $name_tag; then
-        restart_instance $name_tag
+        restart_instance $name_tag || { echo "Error: Could not restart instance."; return 1; }
         echo -n "Instance $instance_id is restarting" >&2
         while [ "$(get_instance_status $name_tag)" != "running" ];
         do
@@ -125,9 +130,8 @@ function connect_to_instance {
     fi
 
     # Connect, using updated PublicDnsName and updated security group settings
-    local dns_name=$(get_instance_public_dns_name ${name_tag})
-    update_security_group_my_ip "${name_tag}"
-    echo "Connecting to ${AWS_EC2_USER_NAME}@${dns_name}" >&2
-    ssh -i ${AWS_EC2_SSH_KEY}${AWS_EC2_USER_NAME}@${dns_name}
+    local dns_name=$(get_instance_public_dns_name ${name_tag}) || { echo "Error: Could not get instance PublicDnsName"; return 1; }
+    update_security_group_my_ip "${name_tag}" || { echo "Error: Could not update security group my ip"; return 1; }
+    echo "Connecting to ${_USER_NAME}@${dns_name}" >&2
+    ssh -i ${_SSH_KEY} ${_USER_NAME}@${dns_name}
 }
-export connect_to_instance
