7a28f1a60ac4fbfcd54c0b5d0c0d343d90576534
Micah Smith <micahjsmith@gmail.com>
2017-01-27 13:05:31 -0500

    Add jupyter notebook config file

---
+++ custom.js
@@ -0,0 +1,16 @@
+// Config for jupyter notebook
+// Skeleton from https://github.com/lambdalisue/jupyter-vim-binding/README.md
+
+// Configure CodeMirror
+require([
+    'nbextensions/vim_binding/vim_binding',  // depends on your installation
+], function() {
+    // Map jk to <Esc>
+    CodeMirror.Vim.map("jk", "<Esc>", "insert");
+    CodeMirror.Vim.map("JK", "<nop>", "insert");
+    // Map ; to :
+    CodeMirror.Vim.map(";", ":", "normal");
+});
+
+// Disable automatic insertion of matching braces, brackets, and paranetheses
+IPython.CodeCell.options_default.cm_config.autoCloseBrackets = false;
