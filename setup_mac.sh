19821aa0f396c833504e8753492285dcf2f9c84b
Micah Smith <micahjsmith@gmail.com>
2017-05-31 16:07:27 -0400

    Check for existing installations on mac install

--- setup_mac.sh
+++ setup_mac.sh
@@ -7,15 +7,19 @@
 echo 
 
 # Config
-if [ -f ~/.bash_profile && $(grep -q 'source ~/.bashrc' ~/.bash_profile) ];
+if [[ -f ~/.bash_profile && $(grep -q 'source ~/.bashrc' ~/.bash_profile) ]];
 then
     # pass
+    true
 else
     echo 'source ~/.bashrc' >> ~/.bash_profile
 fi
 
 # Brew
-/usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+if ! which brew >/dev/null 2>&1;
+then
+    /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+fi
 
 # GNU coreutils
 if ! brew list | grep -q coreutils;
@@ -28,5 +32,5 @@ fi
 # Other brew
 for pkg in tmux wget gnu-which;
 do
-    if ! brew list | grep -q $pkg; then brew install $pkg fi
+    if ! brew list | grep -q $pkg; then brew install $pkg; fi
 done
