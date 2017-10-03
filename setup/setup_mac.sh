8e88e3f5778e7e5845e410a7c925d4d4ab6ffbea
Micah Smith <micahjsmith@gmail.com>
2017-10-02 22:36:12 -0400

    Update

--- setup/setup_mac.sh
+++ setup/setup_mac.sh
@@ -14,7 +14,7 @@ else
 fi
 
 # Brew
-if ! comamnd -v brew >/dev/null 2>&1;
+if ! command -v brew >/dev/null 2>&1;
 then
     /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
 fi
